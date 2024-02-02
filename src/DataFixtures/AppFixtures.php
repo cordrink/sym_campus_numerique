4ff3841e868636081d6977f783c6a7359873b453
cordrink <masodavictorjudicael@gmail.com>
2024-02-02 10:02:55 +0100

    mise en place de AppFixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use EsperoSoft\Faker\Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = new Faker();
+
+        $users = [];
+        for ($i = 0; $i < 15; $i++) {
+            $user = new User();
+            $user->setFullName($faker->full_name())
+                ->setMatricule($faker->email())
+                ->setPassword(sha1("test"))
+                ->setCreatedAt($faker->dateTimeImmutable());
+
+            $users[] = $user;
+            $manager->persist($user);
+        }
+
+        for ($i = 0; $i < 300; $i++) {
+            $event = new Event();
+            $event->setTitle($faker->description(30))
+                ->setCreatedAt($faker->dateTimeImmutable())
+                ->setUser($users[rand(0, count($users) - 1)])
+
+            $manager->persist($event);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
